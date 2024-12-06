96892a605da5ee742d1409dd65e252803bb37415
ParagNirwan <pnirwan9@gmail.com>
2024-12-06 15:04:00 +0100

    Removed index.tsx because main.tsx is used

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import React from "react";
+// Find the root DOM element in the HTML
+const root = document.getElementById("root");
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+if (root) {
+  // Create a React root and render the App component
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found!");
+}
